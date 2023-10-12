0b3aa894d6f6d7062ebe4bf9fb70be2d5d26387e
NIRBHAY ASHVINBHAI VACHHANI <76275418+NirbhayVachhani@users.noreply.github.com>
2023-10-12 08:06:58 -0600

    Add files via upload

---
+++ basic_io_calculation/ex1.py
@@ -0,0 +1,10 @@
+
+
+a = input("Enter your first name: ")
+b = input(("Enter you Last name: "))
+age=int(input("Age: "))
+gpa = float(input("GPA: "))
+major = input("Enter major: ")
+print("Welcome to CPRG216", a,b + "!")
+print("Age",age, "GPA",gpa, "Major",major)
+
---
+++ basic_io_calculation/ex2.py
@@ -0,0 +1,5 @@
+radius = float(input("Enter radius of the circle (cm): "))
+Pi = 3.1415926
+Area = Pi*radius*radius
+Circumference = 2*Pi*radius
+print("Circumference = ",Circumference,"cm","\n", "Area = ",Area,"square cm" )
